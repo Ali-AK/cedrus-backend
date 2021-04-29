e0d2910d2679241fc2051198b11bd6c2dac1ffde
Ali-AK <ali.ak007@hotmail.com>
2021-04-29 11:20:34 +0200

    fixed get bug in movie_rating

--- models/movie_rating.py
+++ models/movie_rating.py
@@ -11,9 +11,12 @@ class MovieRating(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     movie_id = Column(Integer, ForeignKey('movies.id'))
+    rating = Column(Integer)
 
     def get_user_by_id(self):
-        return session.query(User).filter_by(User.id == self.user_id).first()
+        user = session.query(User).filter(User.id == self.user_id).first()
+        return user.username
 
     def get_movie_by_id(self):
-        return session.query(Movie).filter_by(User.id == self.movie_id).first()
+        movie = session.query(Movie).filter(Movie.id == self.movie_id).first()
+        return movie.title
