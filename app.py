2d51613a291b0408f0505aeeca99c0beadd17351
Ali-AK <ali.ak007@hotmail.com>
2021-04-29 11:41:44 +0200

    added CORS

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from database import Base, engine
 import routes
 
 Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
+CORS(app)
 # Register routes blueprints
 app.register_blueprint(routes.movies_router)
 app.register_blueprint(routes.genres_router)
